d2c21d5c994e57caaaf949baccd5e64f00c3569a
Thales Farias Martins <thales.martins@nubank.com.br>
2023-11-01 19:26:08 -0300

    feat: changes datomic library version

--- project.clj
+++ project.clj
@@ -3,9 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :repositories [["my.datomic.com" {:url "https://my.datomic.com/repo"
-                                    :username [:env/my_datomic_username]
-                                    :password [:env/my_datomic_password]}]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.pedestal/pedestal.service "0.5.10"]
                  [io.pedestal/pedestal.route "0.5.10"]
@@ -13,7 +10,7 @@
                  [org.clojure/data.json "2.4.0"]
                  [org.slf4j/slf4j-simple "1.7.36"]
                  [com.stuartsierra/component "1.1.0"]
-                 [com.datomic/datomic-pro "0.9.6045"]
+                 [com.datomic/peer "1.0.7021"]
                  [yogthos/config "1.2.0"]]
   :repl-options {:init-ns heroes-api.main}
   :main ^:skip-aot heroes-api.main
